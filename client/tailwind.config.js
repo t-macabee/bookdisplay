a56b37f3e33fe00db367f029f12bf74e170a6813
Tarik Džakić <tarik.dzakic@outlook.com>
2024-06-25 20:56:03 +0200

    6.0 - Extending the HTML component

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        '3': 'repeat(3, minmax(0, 1fr))',
+      },
+      padding: {
+        'screen-side': '15%',
+      }
+    },
   },
   plugins: [],
 }
