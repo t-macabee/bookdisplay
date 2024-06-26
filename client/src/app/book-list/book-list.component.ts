46ee23689a7bb0cb85fb86d421a3d757d9b16108
Tarik Džakić <tarik.dzakic@outlook.com>
2024-06-26 08:55:27 +0200

    7.0 - Adding pagination and search functionality

--- client/src/app/book-list/book-list.component.ts
+++ client/src/app/book-list/book-list.component.ts
@@ -2,19 +2,27 @@ import {Component, OnInit} from '@angular/core';
 import {Book} from "../_models/book";
 import {BooksService} from "../_services/books.service";
 import {NgForOf, NgOptimizedImage} from "@angular/common";
+import {NgxPaginationModule} from "ngx-pagination";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
   imports: [
     NgForOf,
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgxPaginationModule,
+    FormsModule
   ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
 export class BookListComponent implements OnInit {
   books: Book[] = [];
+  filter: Book[] = [];
+  search: string = '';
+  page: number = 1;
+  pageSize: number = 9;
 
   constructor(private bookService: BooksService) { }
 
@@ -23,6 +31,14 @@ export class BookListComponent implements OnInit {
   }
 
   getBooks() {
-    this.bookService.getAllBooks().subscribe(books => this.books = books);
+    this.bookService.getAllBooks().subscribe(books => {
+      this.books = books;
+      this.filter = books;
+    });
+  }
+
+  searchBooks() {
+    this.filter = this.books.filter(book => book.title.toLowerCase().includes(this.search.toLowerCase()));
+    this.page = 1;
   }
 }
